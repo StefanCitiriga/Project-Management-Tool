f137a2f867f0272839d0b9006f47f760e9123192
StefanRCitiriga <73283856+StefanRCitiriga@users.noreply.github.com>
2024-01-28 16:49:02 +0200

    Very basic interface, input can be saved but cannot be shown yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,53 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSideBar from "./components/ProjectSideBar";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectID: undefined,
+    projects: []
+  });
+  //undefined = we are doing nothing, null = we are adding a new project
+
+  function handleStaratAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectID: null,
+      }
+    })
+  }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return{
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if(projectsState.selectedProjectID === null) {
+    content = <NewProject onAdd={handleAddProject}/>
+  } else if (projectsState.selectedProjectID === undefined) {
+    content= <NoProjectSelected onStartAddProject={handleStaratAddProject}/>
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSideBar onStartAddProject={handleStaratAddProject}/>
+      {content}
+    </main>
   );
 }
 
